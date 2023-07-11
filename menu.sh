0b02ef4616a70b2d4af98bba52bf96a2c65b7c35
Freddo <60254275+srvr-au@users.noreply.github.com>
2023-07-11 17:33:04 +1000

    Add files via upload

--- menu.sh
+++ menu.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-declare -rg btkRed='\e[1;37;41m\e[K'
-declare -rg btkGre='\e[1;37;42m\e[K'
-declare -rg btkYel='\e[1;37;43m\e[K'
-declare -rg btkBlu='\e[1;37;44m\e[K'
-declare -rg btkPur='\e[1;37;45m\e[K'
-declare -rg btkCya='\e[1;37;46m\e[K'
-declare -rg btkLtRed='\e[1;37;101m\e[K'
-declare -rg btkLtGre='\e[1;37;102m\e[K'
-declare -rg btkLtYel='\e[1;37;103m\e[K'
-declare -rg btkLtBlu='\e[1;37;104m\e[K'
-declare -rg btkLtPur='\e[1;37;105m\e[K'
-declare -rg btkLtCya='\e[1;37;106m\e[K'
-declare -rg btkRes='\e[0m'
-declare -rg btkRev='\e[1;7m\e[K'
+declare -rg btkRedBg='\e[1;37;41m\e[K'
+declare -rg btkGreBg='\e[0;30;42m\e[K'
+declare -rg btkYelBg='\e[0;30;43m\e[K'
+declare -rg btkBluBg='\e[1;37;44m\e[K'
+declare -rg btkPurBg='\e[1;37;45m\e[K'
+declare -rg btkCyaBg='\e[0;30;46m\e[K'
+declare -rg btkRedFg='\e[1;31m'
+declare -rg btkGreFg='\e[1;32m'
+declare -rg btkYelFg='\e[1;33m'
+declare -rg btkBluFg='\e[1;34m'
+declare -rg btkPurFg='\e[1;35m'
+declare -rg btkCyaFg='\e[1;36m'
+declare -rg btkReset='\e[0m'
+declare -rg btkReverse='\e[1;7m\e[K'
 declare -rg btkReturn='\n\e[K'
 
 BTKexit(){
   local reason=${1:-Exiting as per your request.}
-  echo -e "\n${btkRed} ${reason}${btkRes}\n"
+  echo -e "\n${btkRedBg} ${reason}${btkReset}\n"
   exit
 }
 
@@ -27,48 +27,45 @@ declare -ag btkMenuOptions
 BTKmenu(){
   local doConfirm=${1:-"y"}
   local heading=${2:-"Menu Heading"}
-  local prompt=${3:-"Press a letter to choose: "}
+  local prompt=${3:-"Type the letter of your choice:"}
   while true; do
-clear
-echo -e "${btkPur} Bash Menu${btkRes}"
-  echo -e "${btkCya} ${heading}${btkRes}"
-  local chars=( {a..p} {r..w} {y..z} )
-  local menuChars=()
-  for i in "${!btkMenuOptions[@]}"; do
-    echo -e "${btkRev} ["${chars[i]}"] - ${btkMenuOptions[$i]}${btkRes}"
-    menuChars+=("${chars[i]}")
-  done
-menuChars+=('q' 'x')
-echo -e "${btkRev} [q] - Quit this Menu${btkRes}"
-echo -e "${btkRev} [x] - Exit this Script${btkRes}"
-  echo -e "${btkCya} ${prompt}${btkRes}"
-  echo -e "${btkPur} ${btkRes}"
-read -n1 -s btkMenuAnswer
-local count=0
-for i in "${menuChars[@]}"; do
-    if [ "$i" == "${btkMenuAnswer}" ] ; then
-      [[ "$i" == 'x' ]] && BTKexit
-      [[ "$i" == 'q' ]] && return
-      if [[ $doConfirm == 'y' ]]; then
-        echo -e "${btkGre} You chose ${btkRes} ${btkMenuOptions[$count]} ${btkGre}, is this correct (y or
- n)${btkRes}"
-        read -n1 -s confirm
-        [[ $confirm == 'y' ]] && return
-      else
+    clear
+    echo -e "${btkCyaBg} ${heading}${btkReset}"
+    local chars=( {a..p} {r..w} {y..z} )
+    local menuChars=()
+    for i in "${!btkMenuOptions[@]}"; do
+      echo -e "${btkGreFg} ["${chars[i]}"] ${btkCyaFg}${btkMenuOptions[$i]}${btkReset}"
+      menuChars+=("${chars[i]}")
+    done
+    menuChars+=('q' 'x')
+    echo -e "${btkGreFg} [q] ${btkCyaFg}Quit this Menu${btkReset}"
+    echo -e "${btkGreFg} [x] ${btkCyaFg}Exit this Script${btkResert}"
+    echo -e "${btkCyaBg} ${prompt}${btkReset}"
+    read -n1 -s btkMenuAnswer
+    local count=0
+    for i in "${menuChars[@]}"; do
+      if [ "$i" == "${btkMenuAnswer}" ] ; then
+        [[ "$i" == 'x' ]] && BTKexit
+        [[ "$i" == 'q' ]] && return
+        if [[ $doConfirm == 'y' ]]; then
+          echo -e "${btkCyaFg}You chose\n${btkGreFg} ${btkMenuOptions[$count]}\n${btkCyaFg}Is this correct (y|n)?${btkReset}"
+          read -n1 -s confirm
+          [[ $confirm == 'y' ]] && return
+        else
         return
       fi
     fi
-((count++))
-done
-done
+    ((count++))
+    done
+  done
 }
 
 while true; do
   btkMenuOptions=('Paste SSH public key into terminal' 'Create SSH Keypair on server' 'I will do it manually later.')
   BTKmenu
   case $btkMenuAnswer in
-    a) echo 'You picked A';;
-    b) echo 'You picked B';;
+    a) echo 'You picked A'; break;;
+    b) echo 'You picked B'; break;;
     c|q) echo 'You did not choose a) or b)'; break;;
   esac
 done
